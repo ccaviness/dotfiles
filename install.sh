37cbe10faaaae05775e1cc2d1574217874c9328f
Clay Caviness <clay@boobah.com>
2024-03-31 14:58:18 -0400

    brew, style

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+# Install homebrew
+if ! brew --version ; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Install Homebrew formulae
 source brew.sh
 
 # Install oh-my-zsh
 source omz.sh
 
-# Install fonts
-source fonts.sh
+# Install fonts and themes
+source style.sh
 
 # Set up symlinks using stow
 source symlinks.sh
--- style.sh
+++ style.sh
@@ -3,23 +3,25 @@
 # Fonts
 pushd ~/Library/Fonts
 declare -a fonts=(
-'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS NF Regular.ttf'
-'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS NF Bold.ttf'
-'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS NF Italic.ttf'
-'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS NF Bold Italic.ttf'
+"https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+"https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
+"https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
+"https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
 )
 
 for font in "${fonts[@]}"; do
-  curl -LO "${font}"
+  curl -fSLO "${font}"
 done
 
-for f in *%20*; do echo ${f//\%20/ }; done
+for f in *%20*; do 
+  mv "${f}" "${f//\%20/ }"
+done
 
-curl -LO https://github.com/arrowtype/recursive/releases/download/v1.085/ArrowType-Recursive-1.085.zip
+curl -fSLO https://github.com/arrowtype/recursive/releases/download/v1.085/ArrowType-Recursive-1.085.zip
 unzip ArrowType-Recursive-1.085.zip
 rm ArrowType-Recursive-1.085.zip
-
 popd
 
 # Themes
-https://raw.githubusercontent.com/catppuccin/sublime-text/main/Catppuccin%20Macchiato.sublime-color-scheme
+curl --output_dir ~ -fsSLO "https://raw.githubusercontent.com/catppuccin/sublime-text/main/Catppuccin%20Macchiato.sublime-color-scheme"
+curl --output_dir ~ -fsSLO "https://raw.githubusercontent.com/catppuccin/iterm/main/colors/catppuccin-macchiato.itermcolors"
