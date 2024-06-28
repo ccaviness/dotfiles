49aff300f7169601f4ab78f51cd484cdf5fc99e2
Clay Caviness <clay@boobah.com>
2024-06-28 10:08:37 -0400

    install bits

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-# Install homebrew
+echo "Install homebrew"
 if ! brew --version ; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Install Homebrew formulae
+echo "Install Homebrew formulae"
 source brew.sh
 
-# Install oh-my-zsh
+echo "Install oh-my-zsh"
 source omz.sh
 
-# Install fonts and themes
+echo "Install fonts and themes"
 source style.sh
 
-# Set up symlinks using stow
+echo "Set up symlinks using stow"
 source symlinks.sh
 
-# # Set up colors/themes
+echo "Set up colors/themes"
 source style.sh
 
 echo "Update symlinks"
